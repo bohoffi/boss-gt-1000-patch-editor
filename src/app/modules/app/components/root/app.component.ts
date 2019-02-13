564b89339ffb0238233ceabcb4bd887fe00d1a99
Basti Hoffmann <bo.hoffi@gmail.com>
2019-02-13 17:46:21 +0100

    initial

---
+++ src/app/modules/app/components/root/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bgt-pe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bossGtPatchEditor';
+}
