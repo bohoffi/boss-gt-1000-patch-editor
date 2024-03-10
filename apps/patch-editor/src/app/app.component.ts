de5550a7258c0534669c1d157fda5b20c8af9251
bohoffi <bo.hoffi@gmail.com>
2024-03-10 15:13:15 +0100

    chore: create poc application

---
+++ apps/patch-editor/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'bgtpe-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
